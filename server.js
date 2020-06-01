c5db34fcf66bbbb745a6d64ce88c1dca18d0bb1a
Martin Šimeček <simecek.m@seznam.cz>
2020-06-01 16:55:29 +0200

    friend-requests event returns users by not only their id

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var serviceAccount = require("./tolkie-service-account-key.json");
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://tolkie-dcedc.firebaseio.com"
+  databaseURL: "https://tolkie-dcedc.firebaseio.com",
 });
 
 const db = firebase.firestore();
@@ -21,27 +21,37 @@ io.use(async (socket, next) => {
       const user = await firebase.auth().verifyIdToken(token);
       socket.userId = user.uid;
       console.log(`${user.email} authenticated!`);
-      next()
+      next();
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
-})
+});
 
 io.on("connection", async (client) => {
   console.log(`client ${client.id} connected`);
 
   client.on("friend-requests", () => {
-    db.collection("friend_requests").where("to", "==", client.userId)
+    db.collection("friend_requests")
+      .where("to", "==", client.userId)
       .get()
-      .then(snapshot => {
-        const data = snapshot.docs.map(doc => doc.data())
-        client.emit("friend-requests", data)
+      .then((snapshot) => {
+        const usersBy = snapshot.docs.map((doc) => doc.data().by);
+        db.collection("users")
+          .where(firebase.firestore.FieldPath.documentId(), "in", usersBy)
+          .get()
+          .then((snapshot) => {
+            const result = snapshot.docs.map((doc) => doc.data());
+            client.emit("friend-requests", result);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       })
-      .catch(error => {
-        console.log(error)
-      })
-  })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 
   client.on("disconnect", () => {
     console.log(`client ${client.id} disconnected`);
